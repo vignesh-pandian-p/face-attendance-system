5ef6d4086ac34298e5e3cc72642e99836ac70fbe
vignesh-pandian-p <vigneshpandian0710@gmail.com>
2025-08-31 22:18:10 +0530

    modified files - minor changes

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-hard-to-guess-string'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://user:password@localhost/face_attendance'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/face_attendance'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
